e4ce14720ed919b103ef3d9ec978f20858fdd21b
Ignacio Barrios <i.barriosp@outlook.es>
2016-11-27 18:45:45 +0100

    Commit inicial: introduccion de fechas correctas con Alert

--- FiestaDeLaMediaNaranja/ViewController.swift
+++ FiestaDeLaMediaNaranja/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fechaAmor: UIDatePicker!
+    @IBOutlet weak var fechaNacimiento: UIDatePicker!
+    
+    var today = NSDate()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -18,6 +23,30 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func changeLoveDate(_ sender: UIDatePicker) {
+        if !fechaValida(fecha: sender.date as NSDate){
+            print("Fecha no válida!")
+        }
+        fechaAmor = sender
+    }
+    func fechaValida(fecha: NSDate) -> Bool {
+        let nacimiento = fechaNacimiento
+        if fecha.earlierDate((nacimiento?.date)! as Date) == fecha as Date {
+            // Creamos un UIAlertController:
+            let alert = UIAlertController(title: "Fecha no válida", message: "No puedes haberte enamorado antes de nacer ;)",
+                                          preferredStyle: UIAlertControllerStyle.alert)
+            
+            alert.addAction(UIAlertAction(title: "Aceptar", style: UIAlertActionStyle.default,handler: {(alert :UIAlertAction!) in
+                self.fechaAmor.date = self.today as Date
+            }))
+            
+            // Presentar el Alert Controller:
+            present(alert, animated: true, completion: nil)
+            return false
+        }
+        return true
     }
 
 
